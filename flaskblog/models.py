92f218d6771205cac604df3ce4652deb76436ffc
George Keloglou <geokeloglou@gmail.com>
2019-06-20 16:50:41 +0300

    reset password functionality

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,5 @@
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
+from itsdangerous import TimedJSONWebSignatureSerializer
 from flask_login import UserMixin
 from datetime import datetime
 
@@ -13,6 +14,19 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = TimedJSONWebSignatureSerializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = TimedJSONWebSignatureSerializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
